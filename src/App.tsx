45421e6a8e27292bc0a1e2069a53e7fd30e555fc
Franciele Tadiello <franciele.p.t@gmail.com>
2025-08-08 11:58:53 -0300

    Estilização simples da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { PokemonList, Pokemon } from "./components/PokemonList";
+import "./App.css";
 
 function App() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -26,64 +27,67 @@ function App() {
   });
 
   return (
-    <div>
+    <div className="app">
       <h1>Pokémons</h1>
-      <input
-        type="text"
-        placeholder="Buscar por nome"
-        value={buscarNome}
-        onChange={(e) => setBuscarNome(e.target.value)}
-      />
+      < div className="filtros">
+        <input
+          type="text"
+          placeholder="Buscar por nome"
+          value={buscarNome}
+          onChange={(e) => setBuscarNome(e.target.value)}
+        />
 
-     <select
-        value={tipoSelecionado}
-        onChange={(e) => setTipoSelecionado(e.target.value)}
-      >
-        <option value="">Todos os tipos</option>
-        <option value="grass">grass</option>
-        <option value="fire">fire</option>
-        <option value="water">water</option>
-        <option value="electric">electric</option>
-        <option value="psychic">psychic</option>
-        <option value="rock">rock</option>
-        <option value="ground">ground</option>
-        <option value="ice">ice</option>
-        <option value="bug">bug</option>
-        <option value="dragon">dragon</option>
-        <option value="ghost">ghost</option>
-        <option value="dark">dark</option>
-        <option value="steel">steel</option>
-        <option value="fairy">fairy</option>
-        <option value="fighting">fighting</option>
-        <option value="normal">normal</option>
-        <option value="poison">poison</option>
-        <option value="flying">flying</option>
-      </select>
+        <select
+          value={tipoSelecionado}
+          onChange={(e) => setTipoSelecionado(e.target.value)}
+        >
+          <option value="">Todos os tipos</option>
+          <option value="grass">grass</option>
+          <option value="fire">fire</option>
+          <option value="water">water</option>
+          <option value="electric">electric</option>
+          <option value="psychic">psychic</option>
+          <option value="rock">rock</option>
+          <option value="ground">ground</option>
+          <option value="ice">ice</option>
+          <option value="bug">bug</option>
+          <option value="dragon">dragon</option>
+          <option value="ghost">ghost</option>
+          <option value="dark">dark</option>
+          <option value="steel">steel</option>
+          <option value="fairy">fairy</option>
+          <option value="fighting">fighting</option>
+          <option value="normal">normal</option>
+          <option value="poison">poison</option>
+          <option value="flying">flying</option>
+        </select>
 
-      <select
-        value={fraquezaSelecionada}
-        onChange={(e) => setFraquezaSelecionada(e.target.value)}
-      >
-        <option value="">Todas as fraquezas</option>
-        <option value="grass">grass</option>
-        <option value="fire">fire</option>
-        <option value="water">water</option>
-        <option value="electric">electric</option>
-        <option value="psychic">psychic</option>
-        <option value="rock">rock</option>
-        <option value="ground">ground</option>
-        <option value="ice">ice</option>
-        <option value="bug">bug</option>
-        <option value="dragon">dragon</option>
-        <option value="ghost">ghost</option>
-        <option value="dark">dark</option>
-        <option value="steel">steel</option>
-        <option value="fairy">fairy</option>
-        <option value="fighting">fighting</option>
-        <option value="normal">normal</option>
-        <option value="poison">poison</option>
-        <option value="flying">flying</option>
-      </select>
+        <select
+          value={fraquezaSelecionada}
+          onChange={(e) => setFraquezaSelecionada(e.target.value)}
+        >
+          <option value="">Todas as fraquezas</option>
+          <option value="grass">grass</option>
+          <option value="fire">fire</option>
+          <option value="water">water</option>
+          <option value="electric">electric</option>
+          <option value="psychic">psychic</option>
+          <option value="rock">rock</option>
+          <option value="ground">ground</option>
+          <option value="ice">ice</option>
+          <option value="bug">bug</option>
+          <option value="dragon">dragon</option>
+          <option value="ghost">ghost</option>
+          <option value="dark">dark</option>
+          <option value="steel">steel</option>
+          <option value="fairy">fairy</option>
+          <option value="fighting">fighting</option>
+          <option value="normal">normal</option>
+          <option value="poison">poison</option>
+          <option value="flying">flying</option>
+        </select>
+
+      </div>
 
       {filtraPokemon.length === 0 ? (
         <p style={{ color: "#888" }}>
--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,27 +1,27 @@
-
+import "../App.css";
 
 export type Pokemon = {
-    id: number;
-    name: string;
-    types: string[];
-    fraquezas: string[];
+  id: number;
+  name: string;
+  types: string[];
+  fraquezas: string[];
 };
 
 interface Props {
-    pokemons: Pokemon[];
+  pokemons: Pokemon[];
 }
 
-export const PokemonList: React.FC<Props> = ({pokemons}) => { 
-  
+export const PokemonList: React.FC<Props> = ({ pokemons }) => {
+
   return (
-        <div>
+    <div className="pokemon-list">
       {pokemons.map((pokemon) => (
-        <div key={pokemon.name} style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}>
+        <div className="pokemon-card" key={pokemon.name}>
           <h2>{pokemon.name}</h2>
           <p><strong>Tipos:</strong> {pokemon.types.join(", ")}</p>
           <p><strong>Fraquezas:</strong> {pokemon.fraquezas.length ? pokemon.fraquezas.join(", ") : "Nenhuma"}</p>
         </div>
       ))}
     </div>
-    );
+  );
 };
